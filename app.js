3858380c204af21c14c4974610708b94ee558437
Osyfliak Ivan <osuflak22@gmail.com>
2023-04-10 15:44:15 +0300

    1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 
@@ -18,12 +19,9 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-
-
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message, })
-})
-
+  res.status(status).json({ message });
+});
 
 module.exports = app;
---
+++ controllers/contacts.js
@@ -0,0 +1,62 @@
+const { HttpError } = require("../helpers");
+
+const { Contact } = require("../models/contact");
+
+const { ctrlWrapper } = require("../utils");
+
+const getAll = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);
+};
+
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json(result);
+};
+
+const add = async (req, res, next) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, {new:true});
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+
+  res.json(result);
+};
+
+const updateFavorite = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, {new:true});
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+
+  res.json(result);
+};
+
+const deleteById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndDelete(id);
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json({ message: "Delete success" });
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  deleteById: ctrlWrapper(deleteById),
+  updateById: ctrlWrapper(updateById),
+  updateFavorite: ctrlWrapper(updateFavorite),
+};
