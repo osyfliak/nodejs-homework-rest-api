745f957561447f3130cf426f4663f8389c0cd2e7
Osyfliak Ivan <osuflak22@gmail.com>
2023-04-11 16:06:18 +0300

    add hw 4

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,19 +8,19 @@ const { validateBody } = require("../../utils");
 
 const {schemas} = require("../../models/contact");
 
-const {isValidId} = require("../../middlewares")
+const {isValidId, authenticate} = require("../../middlewares")
 
-router.get("/", ctrl.getAll);
+router.get("/",authenticate, ctrl.getAll);
 
-router.get("/:id", isValidId, ctrl.getById);
+router.get("/:id",authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+router.post("/",authenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.put("/:id",authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
-router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+router.patch("/:id/favorite",authenticate, isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
-router.delete("/:id", isValidId,  ctrl.deleteById);
+router.delete("/:id",authenticate, isValidId,  ctrl.deleteById);
 
 
 
